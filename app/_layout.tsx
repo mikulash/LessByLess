f3abfe03e966f5c94d014d91635a6f094ac6f6d9
Mikulas Heinz <heinz.mikulas@gmail.com>
2025-09-24 22:54:44 +0200

    fix: correct spelling of ColdTurkey in TrackerType and related components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,62 +1,59 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
-import { TrackedItemsProvider } from '@/contexts/TrackedItemsContext';
+import {useColorScheme} from '@/components/useColorScheme';
+import {TrackedItemsProvider} from '@/contexts/TrackedItemsContext';
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  initialRouteName: 'index',
+    initialRouteName: 'index',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    const colorScheme = useColorScheme();
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+   return (
+       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+           <TrackedItemsProvider>
+               <Stack>
+                   <Stack.Screen name="index" options={{headerShown: false}}/>
+                   <Stack.Screen name="tracker/[id]" options={{title: 'Tracker detail'}}/>
+                   <Stack.Screen name="modal" options={{presentation: 'modal'}}/>
+               </Stack>
+           </TrackedItemsProvider>
+       </ThemeProvider>
+   )
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <TrackedItemsProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="tracker/[id]" options={{ title: 'Tracker detail' }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </TrackedItemsProvider>
-    </ThemeProvider>
-  );
-}
